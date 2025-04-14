b4cbe732f3d0c8c2f5cfe60672a7aad6dd29d964
DESKTOP-PCR3R0O\danihoge <cota2n@gmail.com>
2025-04-14 22:20:18 +0900

    シンプルなWaitAction

---
+++ Scripts/Common/WaitAction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace Selkie.Scripts.Common
+{
+    public class WaitAction : IEnumerator
+    {
+        public WaitAction(Action<Action> action)
+        {
+            action(Callback);
+        }
+
+        private void Callback()
+        {
+            called = true;
+        }
+
+        private bool called;
+
+        public bool MoveNext()
+        {
+            return !called;
+        }
+
+        public void Reset()
+        {
+            throw new NotSupportedException();
+        }
+
+        public object Current => throw new NotSupportedException();
+    }
+}
